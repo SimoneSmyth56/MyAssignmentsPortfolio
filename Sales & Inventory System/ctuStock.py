e835e5a8fc5d88bcf846f247f3a0282f76d9b0f2
Simone Smyth <143381493+SimoneSmyth56@users.noreply.github.com>
2024-05-23 14:00:30 +0200

    Upload Sales & Inventory System

---
+++ Sales & Inventory System/ctuStock.py
@@ -0,0 +1,8 @@
+class Stock:
+    def __init__(self, shopName, shopLocation, customers, sales, returns):
+        self.shopName = shopName
+        self.shopLocation = shopLocation
+        self.customers = customers
+        self.sales = sales
+        self.returns = returns
+
