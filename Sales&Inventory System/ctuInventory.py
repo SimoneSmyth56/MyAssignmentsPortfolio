690b31d8ec6a8cc531a7b349a64c688f8a007182
Simone Smyth <143381493+SimoneSmyth56@users.noreply.github.com>
2024-05-23 14:09:58 +0200

    Delete Sales&Inventory System directory

--- Sales&Inventory System/ctuInventory.py
+++
@@ -1,7 +0,0 @@
-class Inventory:
-    def __init__(self, itemNumber, equipment, quantity, cost):
-        self.itemNumber = itemNumber
-        self.equipment = equipment
-        self.quantity = quantity
-        self.cost = cost
-
