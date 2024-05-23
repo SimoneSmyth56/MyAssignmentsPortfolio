e69466bbfe6bbe46a3412ef8a96681353e014c25
Simone Smyth <143381493+SimoneSmyth56@users.noreply.github.com>
2024-05-23 14:05:56 +0200

    Delete Sales & Inventory System directory

--- Sales & Inventory System/ctuStock.py
+++
@@ -1,8 +0,0 @@
-class Stock:
-    def __init__(self, shopName, shopLocation, customers, sales, returns):
-        self.shopName = shopName
-        self.shopLocation = shopLocation
-        self.customers = customers
-        self.sales = sales
-        self.returns = returns
-
